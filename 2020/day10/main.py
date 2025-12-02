98ca42a99fca52817959215e47ad3370218eff49
MirruK <mirek.kudinoff@hotmail.com>
2025-12-03 00:22:36 +0200

    2020 day 10 part 2

--- 2020/day10/main.py
+++ 2020/day10/main.py
@@ -1,9 +1,25 @@
 from collections import defaultdict
 
 def solve2(A: dict, goal: int):
-    # For every step count number of ways to make 
-    # TODO: Use recursion to solve and then apply dynamic programming
-    pass
+    cache = {}
+    # Use recursion to solve and then apply dynamic programming (memoize with cache dict)
+    # Note: this is exactly the
+    # how many ways to climb a staircase given you can take (1,2 or 3) steps-problem
+    def go(curr: int):
+        if curr == goal:
+            return 1
+        if curr > goal:
+            return 0
+        ls = list(filter(lambda x: x > 0, [
+                A[curr+1],
+                A[curr+2],
+                A[curr+3]]))
+        if curr in cache:
+            return cache[curr]
+        value = sum([go(n) for n in ls])
+        cache[curr] = value
+        return value
+    return go(0)
 
 def solve(A: dict, goal: int):
     B = [0,0,0]
@@ -32,5 +48,7 @@ with open(0) as f:
 goal = max(a.keys())
 
 b = solve(a, goal) 
+c = solve2(a, goal)
 print(b)
+print(c)
     
