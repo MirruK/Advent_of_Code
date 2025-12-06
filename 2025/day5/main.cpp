1e86c088da7c4c63307c451dec87d3bae675b9f3
MirruK <mirek.kudinoff@hotmail.com>
2025-12-06 12:45:54 +0200

    2025 Day 5 part 2

--- 2025/day5/main.cpp
+++ 2025/day5/main.cpp
@@ -1,71 +1,54 @@
+#include <algorithm>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
-#include <optional>
 
 using Range = std::pair<long, long>;
 
-Range record_range(std::string s){
-  long fst = std::stol(s.substr(0,s.find("-")));
-  long snd = std::stol(s.substr(s.find("-")+1,s.length()));
-  return {fst,snd};
+Range record_range(std::string s) {
+  long fst = std::stol(s.substr(0, s.find("-")));
+  long snd = std::stol(s.substr(s.find("-") + 1, s.length()));
+  return {fst, snd};
 }
 
-// Trim overlapping ranges before inserting
+long long count_range(Range r) { return (r.second - r.first) + 1; }
 
+bool in_range(Range r, long val) { return val >= r.first && val <= r.second; }
 
-long sum_range(Range r) {
-  long a0 = r.first;
-  long s1 = (a0*(a0+1)) / 2;
-  long a1 = r.second;
-  long s2 = (a1*(a1+1)) / 2;
-  return s2 - s1;
-}
-
-bool in_range(Range r, long val){
-  return val >= r.first && val <= r.second;
-}
-
-bool ranges_overlap(Range r1, Range r2){
-  if (r1.second < r2.first && r1.first > r2.second ) return false;
+bool ranges_overlap(Range r1, Range r2) {
+  if (r1.second < r2.first)
+    return false;
   return true;
 }
 
-std::optional<Range> merge_ranges (Range r1, Range r2){
-  bool o = ranges_overlap(r1,r2);
+std::optional<Range> merge_ranges(Range r1, Range r2) {
+  bool o = ranges_overlap(r1, r2);
   if (!o) {
     return std::nullopt;
   }
-  if (o && r1.first <= r2.first && r2.second >= r1.second) 
-    return std::optional<Range>{{r1.first, r2.second}};
-  if(o && r2.first <= r1.first && r1.second >= r2.second)
-    return std::optional<Range>{{r2.first, r1.second}};
-  if (o && r1.first <= r2.first && r1.second >= r2.second)
-    return r1;
-  return r2;
+  long bigger2 = r1.second > r2.second ? r1.second : r2.second;
+  return Range{r1.first, bigger2};
 }
 
-std::vector<Range> merge_all_ranges(std::vector<Range> rs){
-  std::vector<Range> new_rs;
+void merge_all_ranges(std::vector<Range> &rs) {
   int merged = 1;
-  while (merged){
-    rs = new_rs;
-    new_rs = {};
+  while (merged) {
     merged = 0;
-  for(int i = 1; i < rs.size(); i++){
-    auto mr = merge_ranges(rs[i-1], rs[i]);
-    if (mr.has_value()) {
-      new_rs.push_back(mr.value());
-      merged++;
+    for (int i = 1; i < rs.size(); i++) {
+      auto mr = merge_ranges(rs[i - 1], rs[i]);
+      if (mr.has_value()) {
+        rs[i] = mr.value();
+        rs.erase(rs.begin() + i - 1);
+        i--;
+        merged++;
+      }
     }
-    else{new_rs.push_back(rs[i-1]);}
   }
-  }
-  return rs;
 }
 
-bool any(const std::vector<Range>& rs, long val){
-  for(const auto& r: rs){
+bool any(const std::vector<Range> &rs, long val) {
+  for (const auto &r : rs) {
     if (in_range(r, val)) {
       return true;
     }
@@ -73,34 +56,34 @@ bool any(const std::vector<Range>& rs, long val){
   return false;
 }
 
-int main(){
+int main() {
   int part = 0;
   std::vector<long> ids;
   std::vector<Range> ranges;
-  for(std::string line; std::getline(std::cin, line);){
-    if(line == ""){
+  for (std::string line; std::getline(std::cin, line);) {
+    if (line == "") {
       part = 1;
       continue;
     }
-    if (part == 0){
+    if (part == 0) {
       ranges.push_back(record_range(line));
-    }
-    else{
+    } else {
       ids.push_back(std::stol(line));
     }
   }
   int sum1 = 0;
   long long sum2 = 0;
-  for(const auto& id: ids){
+  for (const auto &id : ids) {
     if (any(ranges, id)) {
       sum1++;
     }
   }
-  for(auto r: ranges){
-    sum2 += (long long)sum_range(r);
-  }
-  auto rs = merge_all_ranges(ranges);
-  std::cout << "Part 1: " << sum1 << std::endl;
-  std::cout << "Part 2: " << rs.size() << std::endl;
-}
+  std::sort(ranges.begin(), ranges.end());
+  merge_all_ranges(ranges);
 
+  for (auto r : ranges) {
+    sum2 += (long long)count_range(r);
+  }
+  std::cout << "Part 1: " << sum1 << std::endl;
+  std::cout << "Part 2: " << sum2 << std::endl;
+}
