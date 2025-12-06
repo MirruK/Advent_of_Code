b03cd9e318fab89ca8c596eee454a3b27ae0fd23
MirruK <mirek.kudinoff@hotmail.com>
2025-12-06 18:48:58 +0200

    2025 Day 6 part 1

--- 2025/day6/main.cpp
+++ 2025/day6/main.cpp
@@ -7,17 +7,23 @@
 #include <variant>
 #include <vector>
 
-int add(int a, int b) {
-  return a + b;
+
+long long dispatch_fn(const std::vector<long long>& is, int op){
+  long long sum = 0;
+  long long product = 1;
+  for(auto v: is){
+    if (op == 0) {
+      sum += v;
+    } else {
+      product *= v;
+    }
+  }
+  return op == 0 ? sum : product;
 }
 
-int mul(int a, int b) {
-  return a * b;
-}
-static int(*ops_bucket[2])(int, int) = {&add, &mul};
-
-std::optional<int> consume_int(std::string::iterator& s, std::string::iterator end) {
+std::optional<long long> consume_int(std::string::iterator& s, std::string::iterator end) {
   std::string buf;
+  while(std::isspace(*s)){s = std::next(s);}
   while(s != end && !std::isspace(*s)){
     buf.push_back(*s);
     s = std::next(s);
@@ -29,34 +35,42 @@ std::optional<int> consume_int(std::string::iterator& s, std::string::iterator e
 }
 
 int main(){
-  std::optional<int> n;
-  std::map<int, std::vector<int>> num_buckets;
-  std::vector<bool> op_buckets;
-  int i = 0;
-  for (std::string line; std::getline(std::cin, line); i++) {
+  std::optional<long long> n;
+  std::map<int, std::vector<long long>> num_buckets;
+  // std::map<int, std::vector<long long>> num_buckets2;
+  std::vector<short> op_buckets;
+  int col = 0;
+  for (std::string line; std::getline(std::cin, line);) {
     if (line.find("+") != std::string::npos || line.find("*") != std::string::npos) {
       for (auto it = line.begin(); it != line.end(); it++) {
         if(*it == '+'){
-          op_buckets.push_back(false);
+          op_buckets.push_back(0);
+	  col++;
         }
         if(*it == '*'){
-          op_buckets.push_back(true);
-
+          op_buckets.push_back(1);
+	  col++;
         }
-        
       }
+      col = 0;
+      break;
     }
     auto it = line.begin();
     auto end = line.end();
     while((n = consume_int(it, end)).has_value()){
-      if (num_buckets.find(i ) == num_buckets.end()) {
-        num_buckets.insert({i, std::vector<int> {n.value()}});
+      if (num_buckets.find(col) == num_buckets.end()) {
+        num_buckets.insert({col, std::vector<long long> {n.value()}});
       }
       else{
-        num_buckets[i].push_back(n.value());
+        num_buckets[col].push_back(n.value());
       }
+      col++;
     }
+    col = 0;
   }
+  long long sum1 = 0;
+  for(int i = 0; i < op_buckets.size(); i++){
+    sum1 += dispatch_fn(num_buckets[i], op_buckets[i]);
+  }
+  std::cout << "Sum 1: " << sum1 << std::endl;
 }
-
-
