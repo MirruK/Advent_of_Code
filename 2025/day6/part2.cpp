64f69ed8140dc0d9de2b28be23c183a4e9b4e599
MirruK <mirek.kudinoff@hotmail.com>
2025-12-07 13:23:45 +0200

    sync

---
+++ 2025/day6/part2.cpp
@@ -0,0 +1,81 @@
+#include <cctype>
+#include <iterator>
+#include <map>
+#include <optional>
+#include <string>
+#include <iostream>
+#include <variant>
+#include <vector>
+
+
+long operate_on_columns(std::vector<std::string>& lines, int start_col, int col_count, char op){
+  std::string n;
+  long total = op == '+' ? 0 : 1;
+  
+    // For col_start, col_count columns forward
+  for(int col = start_col; col < start_col + col_count; col++){
+      // For every row
+    for(auto& line : lines){
+      // as int
+      // long num = line[col]-'0';
+      n.push_back(line[col]);
+    }
+    long num = std::stol(n);
+    if (op == '+'){
+      total += num;
+    } else {
+      total *= num;
+    }
+    n = "";
+    num = 0;
+  }
+  return total;
+}
+
+int main(){
+  std::optional<long long> n;
+  std::map<int, std::vector<long long>> num_buckets;
+  std::vector<short> op_buckets;
+  int col = 0;
+  std::vector<std::string> lines;
+  for (std::string line; std::getline(std::cin, line);) {
+    lines.push_back(line);
+  }
+  int max = 0;
+  for (int i = 0; i < lines.size(); i++) {
+    if (lines[i].size() >= max) {
+      max = lines[i].size();
+    }
+  }
+  for(auto& line : lines){
+    line.resize(max, ' ');
+  }
+  auto ops = lines.back();
+  lines.pop_back();
+  std::vector<int> col_sizes;
+  std::vector<int> operators;
+  int curr = 0;
+  char curr_op = ops[0];
+  int total = 0;
+  for (auto c : ops){
+    if ((!isspace(c) && curr != 0) || (total == (ops.size() - 1))){
+      col_sizes.push_back(curr);
+      operators.push_back(curr_op);
+      curr_op = c;
+      curr = 0;
+    }
+    curr++;
+    total++;
+  }
+  for(auto cs : col_sizes) {
+    std::cout << cs << std::endl;
+  }
+  col = 0;
+  long sum = 0;
+  for(int i = 0; i < col_sizes.size(); i++){
+    sum += operate_on_columns(lines, col, col_sizes[i], operators[i]);
+    col+=col_sizes[i];
+  }
+  std::cout << "Sum part2: " << sum << std::endl;
+
+}
